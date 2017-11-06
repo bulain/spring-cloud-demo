09c5a5ecb37a8c350f33ef5d81621c31fbc7b802
Bulain Peng <bulain@gmail.com>
2017-11-06 13:56:13 +0800

    update eureka service

--- eureka-service/src/main/java/com/bulain/main/EurekaServiceApplication.java
+++ eureka-service/src/main/java/com/bulain/main/EurekaServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class EurekaClientApplication {
+public class EurekaServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
+        SpringApplication.run(EurekaServiceApplication.class, args);
     }
 
 }
