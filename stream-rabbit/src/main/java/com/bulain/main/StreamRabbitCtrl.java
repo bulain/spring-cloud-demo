aa2cbdb938f67f1c922aaaada7cfadd8ac7461d0
Bulain Peng <bulain@gmail.com>
2018-01-05 23:34:53 +0800

    update stream sender

--- stream-rabbit/src/main/java/com/bulain/main/StreamRabbitCtrl.java
+++ stream-rabbit/src/main/java/com/bulain/main/StreamRabbitCtrl.java
@@ -3,6 +3,7 @@ package com.bulain.main;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class StreamRabbitCtrl {
 
 	@Autowired
-	@Qualifier("output")
+	@Qualifier(Source.OUTPUT)
 	MessageChannel output;
 
 	@RequestMapping("/send")
