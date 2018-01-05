46610889783817c51fe4dcf13cf0c09e4f5f059d
Bulain Peng <bulain@gmail.com>
2018-01-05 22:23:11 +0800

    add stream kafka and rabbit

---
+++ stream-kafka/src/main/java/com/bulain/main/StreamKafkaApplication.java
@@ -0,0 +1,15 @@
+package com.bulain.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class StreamKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamKafkaApplication.class, args);
+    }
+
+}
