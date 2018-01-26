96eca4fa965b9fddd209d97a3bff0c6e0038d96c
Bulain Peng <bulain@gmail.com>
2018-01-26 16:52:26 +0800

    update ziplin demo

--- zipkin-server/src/main/java/com/bulain/main/ZiplinServerApplication.java
+++ zipkin-server/src/main/java/com/bulain/main/ZiplinServerApplication.java
@@ -1,18 +1,33 @@
 package com.bulain.main;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 import zipkin.server.EnableZipkinServer;
+import zipkin.storage.mysql.MySQLStorage;
 
+//@EnableZipkinStreamServer
 @EnableZipkinServer
 @EnableEurekaClient
 @SpringBootApplication
 public class ZiplinServerApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(ZiplinServerApplication.class, args);
     }
 
+    private static final ExecutorService executor = Executors.newFixedThreadPool(1);
+    
+    @Bean
+    public MySQLStorage mySQLStorage(DataSource datasource) {
+        return MySQLStorage.builder().datasource(datasource).executor(executor).build();
+    }
+    
 }
