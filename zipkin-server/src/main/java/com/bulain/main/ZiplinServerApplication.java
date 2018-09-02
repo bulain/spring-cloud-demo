8e7f996a557d7d9db202e4de65fc1fe4a1e7ecee
Bulain Peng <bulain@gmail.com>
2018-09-02 21:14:58 +0800

    fix spring cloud

--- zipkin-server/src/main/java/com/bulain/main/ZiplinServerApplication.java
+++ zipkin-server/src/main/java/com/bulain/main/ZiplinServerApplication.java
@@ -1,33 +1,11 @@
 package com.bulain.main;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import zipkin.server.ZipkinServer;
 
-import javax.sql.DataSource;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-
-import zipkin.server.EnableZipkinServer;
-import zipkin.storage.mysql.MySQLStorage;
-
-//@EnableZipkinStreamServer
-@EnableZipkinServer
-@EnableEurekaClient
-@SpringBootApplication
 public class ZiplinServerApplication {
-    
-    public static void main(String[] args) {
-        SpringApplication.run(ZiplinServerApplication.class, args);
-    }
 
-    private static final ExecutorService executor = Executors.newFixedThreadPool(1);
-    
-    @Bean
-    public MySQLStorage mySQLStorage(DataSource datasource) {
-        return MySQLStorage.builder().datasource(datasource).executor(executor).build();
-    }
-    
+	public static void main(String[] args) {
+		ZipkinServer.main(args);
+	}
+
 }
--- zipkin-tx/src/main/java/com/bulain/main/ZipkinTxApplication.java
+++ zipkin-tx/src/main/java/com/bulain/main/ZipkinTxApplication.java
@@ -3,7 +3,7 @@ package com.bulain.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableEurekaClient
