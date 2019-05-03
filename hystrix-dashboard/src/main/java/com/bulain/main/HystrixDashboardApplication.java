e49431f9cc451f8f12c050b8d1e28b1e3193fbdb
Bulain Peng <bulain@gmail.com>
2019-05-04 00:46:25 +0800

    seperate hystrix-dashboard and turbine-server

--- hystrix-dashboard/src/main/java/com/bulain/main/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/bulain/main/HystrixDashboardApplication.java
@@ -2,18 +2,16 @@ package com.bulain.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@EnableCircuitBreaker
-@EnableFeignClients
+@EnableHystrixDashboard
 @EnableDiscoveryClient
 @SpringBootApplication
-public class HystrixClientApplication {
+public class HystrixDashboardApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HystrixClientApplication.class, args);
+        SpringApplication.run(HystrixDashboardApplication.class, args);
     }
 
 }
--- turbine-server/src/main/java/com/bulain/main/TurbineServerApplication.java
+++ turbine-server/src/main/java/com/bulain/main/TurbineServerApplication.java
@@ -2,18 +2,16 @@ package com.bulain.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
-@EnableCircuitBreaker
-@EnableFeignClients
+@EnableTurbine
 @EnableDiscoveryClient
 @SpringBootApplication
-public class HystrixClientApplication {
+public class TurbineServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(HystrixClientApplication.class, args);
+        SpringApplication.run(TurbineServerApplication.class, args);
     }
 
 }
