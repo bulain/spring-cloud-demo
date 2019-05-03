c7b7dabb0065f426101488c33aa0dcd13a9f18ec
Bulain Peng <bulain@gmail.com>
2019-05-03 12:06:14 +0800

    fix work missing

--- eureka-service/src/main/java/com/bulain/main/EurekaServiceCtrl.java
+++ eureka-service/src/main/java/com/bulain/main/EurekaServiceCtrl.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @EnableAutoConfiguration
-public class EureKaServiceCtrl {
+public class EurekaServiceCtrl {
 
 	@Value("${server.port}")
 	String port;
